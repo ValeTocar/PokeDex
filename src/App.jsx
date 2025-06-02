081310df81502fcc0fa2b854b601ada5325e63d4
AngelGuaricuco <angelguaricuco@gmail.com>
2025-06-01 23:35:16 -0400

    PokeDex in the Process

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Pokedex</h1>
+      <p>Proximamente</p>
+    </>
+  )
+}
+
+export default App
